5ad6be5dc3da89415f2c7df124ffd1536ed6dbae
AMAN YADAV <ayadav@sefas.com>
2024-02-12 19:55:12 +0100

    first one

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 include("connection.php");
  
 if(isset($_POST['but_upload'])){
-   $maxsize = 1005242880; // 5MB
+   $maxsize = 1005242880;
    if(isset($_FILES['file']['name']) && $_FILES['file']['name'] != ''){
        $name = $_FILES['file']['name'];
-       $target_dir = "videos/";
+       $target_dir = "files/";
        $target_file = $target_dir . $_FILES["file"]["name"];
 
        // Select file type
        $extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
        // Valid file extensions
-       $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
+       $extensions_arr = array("mp4","avi","3gp","mov","mpeg", "mp3", "wav", "borvis", "pdf");
 
        // Check extension
        if( in_array($extension,$extensions_arr) ){
  
           // Check file size
           if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-             $_SESSION['message'] = "File too large. File must be less than 5MB.";
+             $_SESSION['message'] = "File too large.";
           }else{
              // Upload
              if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
@@ -27,7 +27,7 @@ if(isset($_POST['but_upload'])){
                $query = "INSERT INTO account2(name,location) VALUES('".$name."','".$target_file."')";
 
                mysqli_query($connection,$query);
-               $_SESSION['message'] = "Upload successfully.";
+               $_SESSION['message'] = "Uploaded successfully.";
              }
           }
 
@@ -45,21 +45,44 @@ if(isset($_POST['but_upload'])){
 <!doctype html> 
 <html> 
   <head>
-     <title>Video Upload</title>
+     <title>File Upload</title>
   </head>
-  <body>
+  <style>
+   body {
+  background-color: transparent;
+}
 
-    <!-- Upload response -->
+h1{
+   text-align: center;
+   font-size: 30px;
+   margin-top: 4%;
+}
+
+form {
+   align-items: center;
+   text-align: center;
+  color: maroon;
+  margin-top: 4%;
+}
+
+input {
+   margin-top: 2%;
+   background-color: transparent;
+}
+
+</style>
+  <body>
     <?php 
     if(isset($_SESSION['message'])){
        echo $_SESSION['message'];
        unset($_SESSION['message']);
     }
     ?>
+    <img src="./assets/logo.ico" alt="mylogo" width="150" height="120"><br>
+    <h1>3D Music Converter</h1>
     <form method="post" action="" enctype='multipart/form-data'>
-      <input type='file' name='file' />
-      <input type='submit' value='Upload' name='but_upload'>
+      <input type='file' name='file' /><br>
+      <input type='submit' value='Convert' name='but_upload'>
     </form>
-
   </body>
 </html>
